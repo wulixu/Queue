25230cf036419e3ad8212c2d4790ddbc83620346
zhaobaoguang <zhoubaoguang@hotmail.com>
2014-08-03 18:47:10 +0800

    api和收料页面

--- TronCell.Queue.Web/Areas/Receiver/Controllers/ChoiceAreaController.cs
+++ TronCell.Queue.Web/Areas/Receiver/Controllers/ChoiceAreaController.cs
@@ -31,44 +31,56 @@ namespace TronCell.Queue.Web.Areas.Receiver.Controllers
             QueueCall queEntity = new QueueCall();
             DateTime today = DateTime.Parse(DateTime.Now.ToString("yyyy-MM-dd"));
             DateTime tomorrow = today.AddDays(1);
+            bool resutl = true;
             if (methodName != "firstQueue")
             {
-                PassQueue(today, tomorrow, methodName, queueCallId);
+                resutl= PassQueue(today, tomorrow, methodName, queueCallId);
             }
-            var querylist = (from p in db.Queues
-                             where p.QueueNum != null && p.CreateTime >= today && p.CreateTime < tomorrow && p.State == TronCell.Queue.Web.Models.ProcessStatus.GotQueueNumber && p.State != TronCell.Queue.Web.Models.ProcessStatus.Processed && p.State != TronCell.Queue.Web.Models.ProcessStatus.NoQueueNumber && p.State != TronCell.Queue.Web.Models.ProcessStatus.LazyProcess && p.Deleted == false
-                             select new { p.QueueCallId, p.QueueNum, p.Priority}).OrderBy(w => w.Priority).ThenBy(v => v.QueueNum).ToList();
-
-            if (querylist.Count > 0)
+            if (resutl == true)
             {
-                var item = querylist.ToList()[0];
-                if (item.QueueNum != "")
+                var querylist = (from p in db.Queues
+                                 where p.QueueNum != null && p.CreateTime >= today && p.CreateTime < tomorrow && p.State == TronCell.Queue.Web.Models.ProcessStatus.GotQueueNumber && p.State != TronCell.Queue.Web.Models.ProcessStatus.Processed && p.State != TronCell.Queue.Web.Models.ProcessStatus.NoQueueNumber && p.State != TronCell.Queue.Web.Models.ProcessStatus.LazyProcess && p.Deleted == false
+                                 select new { p.QueueCallId, p.QueueNum, p.Priority }).OrderBy(w => w.Priority).ThenBy(v => v.QueueNum).ToList();
+
+                if (querylist.Count > 0)
                 {
-                    queEntity = db.Queues.Find(item.QueueCallId);
-                    if (queEntity.State != TronCell.Queue.Web.Models.ProcessStatus.Processing)
+                    var item = querylist.ToList()[0];
+                    if (item.QueueNum != "")
                     {
-                        queEntity.State = TronCell.Queue.Web.Models.ProcessStatus.Processing;
+                        queEntity = db.Queues.Find(item.QueueCallId);
+                        if (queEntity.State != TronCell.Queue.Web.Models.ProcessStatus.Processing)
+                        {
+                            queEntity.State = TronCell.Queue.Web.Models.ProcessStatus.Processing;
+                        }
+                        queEntity.ReceiveArea = receivearea;
+                        var strContent = string.Format("顾客您好，请到{0}进行收料！", receivearea.AreaName.ToString());
+                        sendSMS(queEntity.QueueUser.PhoneNumber, strContent);
+
+                        db.Entry(queEntity).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
                     }
-                    queEntity.ReceiveArea = receivearea;
-                    var strContent=string.Format("顾客您好，请到{0}进行收料！",receivearea.AreaName.ToString());
-                    sendSMS(queEntity.QueueUser.PhoneNumber, strContent);
                 }
-                db.Entry(queEntity).State = System.Data.Entity.EntityState.Modified;
-                db.SaveChanges();
+            }
+            else
+            {
+                queEntity.QueueCallId = queueCallId;
             }
             QueueCall queueDetails = db.Queues.Find(queEntity.QueueCallId);
             if (queueDetails != null)
                 return View(queueDetails);
             else
-                return View();
+                //return RedirectToAction("TimeRefreshPage", "ChoiceArea", new { area = "Receiver", AreaID = id });
+                return RedirectToAction("TimeRefreshPage",new { AreaID = id });
         }
-        public ActionResult text()
+        public ActionResult TimeRefreshPage(int AreaID)
         {
+            ViewBag.valueAreaid = AreaID;
             return View();
         }
         //处理是否收料
-        private void PassQueue(DateTime today, DateTime tomorrow, string methodName, int queueCallId)
+        private bool PassQueue(DateTime today, DateTime tomorrow, string methodName, int queueCallId)
         {
+            bool result = true;
             try
             {
                 var userName = User.Identity.Name;
@@ -76,26 +88,44 @@ namespace TronCell.Queue.Web.Areas.Receiver.Controllers
                              where p.UserName == userName
                              select new { p.Id, p.IDCard, p.UserName, p.Roles }).ToList();
                 QueueCall queue = db.Queues.Find(queueCallId);
+
                 if (queue != null)
                 {
-                    if (methodName == "PassQueue")
-                        queue.Deleted = true;
-                    else if (methodName == "Problematic")
-                        queue.State = TronCell.Queue.Web.Models.ProcessStatus.LazyProcess;
-                    else if (methodName == "NextQueue")
-                        queue.State = TronCell.Queue.Web.Models.ProcessStatus.Processed;
+                    switch (methodName)
+                    {
+                        case "PassQueue":
+                            queue.Deleted = true;
+                            queue.EndTime = DateTime.Now;
+                            break;
+                        case "Problematic":
+                            queue.State = TronCell.Queue.Web.Models.ProcessStatus.LazyProcess;
+                            queue.EndTime = DateTime.Now;
+                            break;
+                        case "NextQueue":
+                            if(queue.StartTime!=null)
+                            {
+                                queue.State=TronCell.Queue.Web.Models.ProcessStatus.Processed;
+                                queue.EndTime=DateTime.Now;
+                            }
+                            break;
+                        case "startRecive":
+                            queue.StartTime = DateTime.Now;
+                            result = false;
+                            break;
+                    }
+                    if (query.Count > 0)
+                    {
+                        queue.OperationId = query[0].Id;
+                        db.Entry(queue).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                    }
                 }
-                if (query.Count > 0)
-                {
-                    queue.OperationId = query[0].Id;
-                }
-                db.Entry(queue).State = System.Data.Entity.EntityState.Modified;
-                db.SaveChanges();
             }
             catch (Exception ex)
             {
                 string errStr = ex.Message;
             }
+            return result;
         }
 
         private string sendSMS(string mobile, string strContent)
