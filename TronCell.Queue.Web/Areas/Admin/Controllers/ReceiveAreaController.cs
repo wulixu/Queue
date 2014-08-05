ed3737cee5c2c37ff5be42c40075494dbca2110f
zhaobaoguang <zhoubaoguang@hotmail.com>
2014-08-05 16:55:19 +0800

    ReceiverArea Controller update

--- TronCell.Queue.Web/Areas/Admin/Controllers/ReceiveAreaController.cs
+++ TronCell.Queue.Web/Areas/Admin/Controllers/ReceiveAreaController.cs
@@ -17,15 +17,16 @@ namespace TronCell.Queue.Web.Areas.Admin.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: /Admin/ReceiveArea/
+        // GET: /Admin/ReceiverArea/
         public async Task<ActionResult> Index(int pageIndex = 1)
         {
-            var model=db.ReceiveArea.AsQueryable().OrderBy(a => a.AreaName).ToPagedList(pageIndex, 10);
+            var model = db.ReceiveArea.AsQueryable().OrderBy(a => a.AreaName).ToPagedList(pageIndex, 10);
             return View(model);
             //return View(await db.ReceiveArea.ToListAsync());
         }
 
-        // GET: /Admin/ReceiveArea/Details/5
+
+        // GET: /Admin/ReceiverArea/Details/5
         public async Task<ActionResult> Details(int? id)
         {
             if (id == null)
@@ -40,18 +41,18 @@ namespace TronCell.Queue.Web.Areas.Admin.Controllers
             return View(receivearea);
         }
 
-        // GET: /Admin/ReceiveArea/Create
+        // GET: /Admin/ReceiverArea/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: /Admin/ReceiveArea/Create
+        // POST: /Admin/ReceiverArea/Create
         // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
         // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include="ReceiveAreaId,AreaName,Description,Category")] ReceiveArea receivearea)
+        public async Task<ActionResult> Create([Bind(Include="ReceiveAreaId,AreaName,Description,Category,CreateTime")] ReceiveArea receivearea)
         {
             if (ModelState.IsValid)
             {
@@ -65,7 +66,7 @@ namespace TronCell.Queue.Web.Areas.Admin.Controllers
             return View(receivearea);
         }
 
-        // GET: /Admin/ReceiveArea/Edit/5
+        // GET: /Admin/ReceiverArea/Edit/5
         public async Task<ActionResult> Edit(int? id)
         {
             if (id == null)
@@ -80,12 +81,12 @@ namespace TronCell.Queue.Web.Areas.Admin.Controllers
             return View(receivearea);
         }
 
-        // POST: /Admin/ReceiveArea/Edit/5
+        // POST: /Admin/ReceiverArea/Edit/5
         // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
         // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include="ReceiveAreaId,AreaName,Description,Category")] ReceiveArea receivearea)
+        public async Task<ActionResult> Edit([Bind(Include="ReceiveAreaId,AreaName,Description,Category,CreateTime")] ReceiveArea receivearea)
         {
             if (ModelState.IsValid)
             {
@@ -96,24 +97,8 @@ namespace TronCell.Queue.Web.Areas.Admin.Controllers
             return View(receivearea);
         }
 
-        // GET: /Admin/ReceiveArea/Delete/5
-        public async Task<ActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ReceiveArea receivearea = await db.ReceiveArea.FindAsync(id);
-            if (receivearea == null)
-            {
-                return HttpNotFound();
-            }
-            return View(receivearea);
-        }
-
-        // POST: /Admin/ReceiveArea/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
+        [HttpPost]
+        [ActionName("Delete")]
         public async Task<ActionResult> DeleteConfirmed(int id)
         {
             ReceiveArea receivearea = await db.ReceiveArea.FindAsync(id);
